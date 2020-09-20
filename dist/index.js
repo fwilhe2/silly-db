073a0c364c0dfd745a128a393b73977f88e91ddb
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-09-20 17:08:25 +0200

    update

--- dist/index.js
+++ dist/index.js
@@ -43,16 +43,21 @@ function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const key = core.getInput('key');
-            core.debug(`Query for ${key}`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-            const fixture = `{
+            core.info(`Query for ${key}`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+            if (fs.existsSync('db.json')) {
+                core.info("Found existing db");
+            }
+            else {
+                const fixture = `{
       "Germany": "Berlin",
       "France": "Paris",
       "Italy": "Rome"
     }`;
-            fs.writeFileSync('db.json', fixture);
+                fs.writeFileSync('db.json', fixture);
+            }
             const input = fs.readFileSync('db.json');
             const data = JSON.parse(input.toString());
-            core.debug(`Value for key ${key}: ${data[key]}`);
+            core.info(`Value for key ${key}: ${data[key]}`);
             core.setOutput('value', data[key]);
         }
         catch (error) {
--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,24 @@ import * as fs from 'fs'
 async function run(): Promise<void> {
   try {
     const key: string = core.getInput('key')
-    core.debug(`Query for ${key}`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    core.info(`Query for ${key}`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    const fixture = `{
+    if (fs.existsSync('db.json')) {
+      core.info('Found existing db')
+    } else {
+      const fixture = `{
       "Germany": "Berlin",
       "France": "Paris",
       "Italy": "Rome"
     }`
 
-    fs.writeFileSync('db.json', fixture)
+      fs.writeFileSync('db.json', fixture)
+    }
 
     const input = fs.readFileSync('db.json')
     const data = JSON.parse(input.toString())
 
-    core.debug(`Value for key ${key}: ${data[key]}`)
+    core.info(`Value for key ${key}: ${data[key]}`)
 
     core.setOutput('value', data[key])
   } catch (error) {
