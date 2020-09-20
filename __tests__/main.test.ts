a4d1693d5b130891a701eace57549e3137f285b7
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-09-20 16:48:10 +0200

    First fixture

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,22 +3,20 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
+const toolDir = path.join(__dirname, 'runner', 'tools')
+const tempDir = path.join(__dirname, 'runner', 'temp')
+
+process.env['RUNNER_TOOL_CACHE'] = toolDir
+process.env['RUNNER_TEMP'] = tempDir
+
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
 })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_KEY'] = 'Italy'
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+// import {wait} from './wait'
+import * as fs from 'fs'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const key: string = core.getInput('key')
+    core.debug(`Query for ${key}`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const fixture = `{
+      "Germany": "Berlin",
+      "France": "Paris",
+      "Italy": "Rome"
+    }`
 
-    core.setOutput('time', new Date().toTimeString())
+    fs.writeFileSync('db.json', fixture)
+
+    const input = fs.readFileSync('db.json')
+    const data = JSON.parse(input.toString())
+
+    core.debug(`Value for key ${key}: ${data[key]}`)
+
+    core.setOutput('value', data[key])
   } catch (error) {
     core.setFailed(error.message)
   }
